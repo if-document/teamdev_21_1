e5ef3583b8cec9b95b897495d1c07d8f60ddfd0e
mikikoike <mikikoike1@gmail.com>
2025-10-09 23:49:04 +0900

    記事詳細取得

--- src/app/article/[id]/page.tsx
+++ src/app/article/[id]/page.tsx
@@ -1,13 +1,36 @@
 import { ArticleSection } from "@/components/article/ArticleSection";
 import { CommentSection } from "@/components/article/CommentSection";
-import { articleData, commentListData } from "@/lib/sampleData";
+import { getArticleById } from "@/lib/api/articles";
+import { getCommentsByArticleId } from "@/lib/api/comments";
+import { notFound } from "next/navigation";
 
-export default function ArticlePage() {
+export default async function ArticlePage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const articleId = parseInt(id, 10);
+
+  if (isNaN(articleId)) {
+    notFound();
+  }
+
+  const [article, comments] = await Promise.all([
+    getArticleById(articleId),
+    getCommentsByArticleId(articleId),
+  ]);
+
+  if (!article) {
+    notFound();
+  }
+
   return (
     <div className="min-h-screen bg-white flex flex-col pt-[60px] pb-[35px]">
-      {/* Navbar */}
-      <ArticleSection article={articleData} />
-      <CommentSection comments={commentListData} />
+      <ArticleSection article={article} />
+      <CommentSection comments={comments} />
     </div>
   );
 }
+
+export const revalidate = 3600;
